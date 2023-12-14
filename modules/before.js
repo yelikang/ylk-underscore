33b364b3eae495fe9cdceb0e68150481e7c69bbb
yelikang <530331059@qq.com>
2023-12-14 09:41:11 +0800

    before 函数执行到多少次之后不执行，源码分析

--- modules/before.js
+++ modules/before.js
@@ -1,5 +1,7 @@
 // Returns a function that will only be executed up to (but not including) the
 // Nth call.
+// 返回一个函数，该函数只执行到(但不包括)，第n次调用
+//  之后再调用这个函数，将返回一次最后调用func的结果。
 export default function before(times, func) {
   var memo;
   return function() {
