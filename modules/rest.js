20f37c45a414b27b1c421e3739167757e427cd7d
yelikang <530331059@qq.com>
2023-12-14 09:41:43 +0800

    rest/drop丢弃数组内容，源码分析

--- modules/rest.js
+++ modules/rest.js
@@ -1,8 +1,13 @@
-import { slice } from './_setup.js';
+import { slice } from './_setup.js'
 
 // Returns everything but the first entry of the `array`. Especially useful on
 // the `arguments` object. Passing an **n** will return the rest N values in the
 // `array`.
+
+// 传入一个数组
+// 返回剔除第一个元素后的数组副本
+// 如果传入参数 n，则剔除前 n 个元素
+// 也命名为drop
 export default function rest(array, n, guard) {
-  return slice.call(array, n == null || guard ? 1 : n);
+    return slice.call(array, n == null || guard ? 1 : n)
 }
