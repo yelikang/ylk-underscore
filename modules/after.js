7745dc8fca01969e03d937d7cb73ec17299c99c3
yelikang <530331059@qq.com>
2023-12-14 09:40:35 +0800

    after 函数执行多少次之后才执行，码分析

--- modules/after.js
+++ modules/after.js
@@ -1,4 +1,6 @@
 // Returns a function that will only be executed on and after the Nth call.
+// 返回一个函数，该函数只在第n次调用时和之后执行。
+// 可以结合webpack的needCalls函数，进行异常机制处理
 export default function after(times, func) {
   return function() {
     if (--times < 1) {
